4c5e0d0c759cbf2d55edd44f95d27843891b2db6
francisquinha <francisquinha@gmail.com>
2015-10-26 18:56:36 +0000

    half vs whole

---
+++ dominup/dominup.pl
@@ -0,0 +1,39 @@
+/* -*- Mode:Prolog; coding:utf-8; -*- */
+
+/*piece(Line, Column, Level, Number1, Number2, Cardinal).*/
+
+piece(12, 11, 1, 1, 1, s).
+piece(12, 12, 1, 7, 7, e).
+piece(13, 13, 2, 4, 7, n).
+piece(14, 13, 1, 2, 4, n).
+piece(14, 13, 2, 2, 3, s).
+piece(14, 15, 1, 0, 5, o).
+piece(14, 13, 3, 2, 5, e).
+piece(15, 14, 2, 3, 5, n).
+piece(15, 13, 1, 3, 3, e).
+piece(14, 16, 1, 6, 6, s).
+
+/*halfPiece(Line, Column, Level, Number, Cardinal).*/
+
+halfPiece(12, 11, 1, 1, s).
+halfPiece(13, 11, 1, 1, n).
+halfPiece(12, 12, 1, 7, e).
+halfPiece(12, 13, 1, 7, o).
+halfPiece(13, 13, 2, 4, n).
+halfPiece(12, 13, 2, 7, s).
+halfPiece(14, 13, 1, 2, n).
+halfPiece(13, 13, 1, 4, s).
+halfPiece(15, 13, 1, 3, e).
+halfPiece(15, 14, 1, 3, o).
+halfPiece(14, 13, 2, 2, s).
+halfPiece(15, 13, 2, 3, n).
+halfPiece(15, 14, 2, 3, n).
+halfPiece(14, 14, 2, 5, s).
+halfPiece(14, 13, 3, 2, e).
+halfPiece(14, 14, 3, 5, o).
+halfPiece(14, 15, 1, 0, o).
+halfPiece(14, 14, 1, 5, e).
+halfPiece(14, 16, 1, 6, s).
+halfPiece(15, 16, 1, 6, n).
+
+
