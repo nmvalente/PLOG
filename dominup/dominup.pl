158de80be9c9e1cc13f52697ea83ba06c0a53f4e
francisquinha <francisquinha@gmail.com>
2015-11-01 00:02:52 +0000

    Print Player
    alterei algumas coisas no display do tabuleiro, essencialmente por
    questões de consistência.
    
    criei o display das peças de um jogador. na sua forma atual, é um
    bocado extenso e difícil de entender. aquilo que fiz para chegar a este
    bicho foi ir construindo cada predicado aos poucos. até que cheguei a
    um ponto, que para ter as peças em duas linhas, tive que juntar um
    monte de coisas... não sei se dá para fazer algum refactoring que torne
    isto mais compreensível.
    
    em vez de listas, usei um predicado para as peças dos jogadores. para
    alterar uma peça de não jogada para jogada, teremos que usar retracts.
    
    bem, o melhor é falarmos sobre tudo isto no skype ou assim...

--- dominup/dominup.pl
+++ dominup/dominup.pl
@@ -1,54 +1,110 @@
 /* -*- Mode:Prolog; coding:utf-8; -*- */
 
+
+/****************/
 /* useful stuff */
+/****************/
 not(P) :- (call(P) -> fail ; true).
 
-/* board display */
-displayBoard :- nl, putLetters, nl, putGridTop, nl, row(1), putLetters, nl, nl,!.
 
-row(N) :- ((N < 25, putLeftNumbers(N), analyseCell(N, 1), putRightNumbers(N), nl, putGrid(N), N1 is N + 1, nl, row(N1)) ; !).
+/***************/
+/* print board */
+/***************/
 
-analyseCell(_, 25) :- !.
-analyseCell(25, _) :- !.
-analyseCell(X, Y) :- displayCell(X, Y),  write('│'), Y1 is Y + 1, analyseCell(X, Y1).
+printBoard :- nl, printLetters, nl, printGridTop, nl, printRows(1), printLetters, nl, nl,!.
 
-displayCell(X, Y) :- getTopLevel(X, Y, L), (L > 0 -> halfPiece(X, Y, L, N, C), print(N), displayCardinal(C), print(L); write('   ')).
+printRows(25) :- !.
+printRows(X) :- printLeftNumbers(X), printCells(X, 1), printRightNumbers(X), nl, printGrid(X), X1 is X + 1, nl, printRows(X1).
+
+printCells(_,25) :- !.
+printCells(X, Y) :- getTopLevel(X, Y, L), 
+                     (L > 0 -> halfPiece(X, Y, L, N, C), print(N), printCardinal(C), print(L); write('   ')), 
+                     write('│'), Y1 is Y + 1, printCells(X, Y1).
 
 getTopLevel(X, Y, 0) :- not(halfPiece(X, Y, _, _, _)).
 getTopLevel(X, Y, L) :- halfPiece(X, Y, L, _, _), L1 is L + 1, not(halfPiece(X, Y, L1, _, _)).
 
-displayCardinal(n) :- write('↑').
-displayCardinal(e) :- write('→').
-displayCardinal(s) :- write('↓').
-displayCardinal(w) :- write('←').
+printCardinal(n) :- write('↑').
+printCardinal(e) :- write('→').
+printCardinal(s) :- write('↓').
+printCardinal(w) :- write('←').
 
-putLetters :- write('    A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   Y   X').
+printLetters :- write('    A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   Y   X').
 
-putLeftNumbers(N) :- ((N < 10, write(0), write(N), write('│')) ; (write(N), write('│'))).
+printLeftNumbers(X) :- ((X < 10, write(0), write(X), write('│')) ; (write(X), write('│'))).
 
-putRightNumbers(N) :- ((N < 10, write(0), write(N)) ; (write(N))).
+printRightNumbers(X) :- ((X < 10, write(0), write(X)) ; (write(X))).
 
-putGridTop :- write('  ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐').
+printGridTop :- write('  ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐').
 
-putGrid(N) :-((N < 24, 
-              write('  ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤'));
-             (write('  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘'))).
+printGrid(X) :-((X < 24, 
+                write('  ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤'));
+                write('  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘')).
 
 
+/****************/
+/* print player */
+/****************/
 
-/*piece(Line, Column, Level, Number1, Number2, Cardinal).*/
-/*
-piece(12, 11, 1, 1, 1, s).
-piece(12, 12, 1, 7, 7, e).
-piece(13, 13, 2, 4, 7, n).
-piece(14, 13, 1, 2, 4, n).
-piece(14, 13, 2, 2, 3, s).
-piece(14, 15, 1, 0, 5, w).
-piece(14, 13, 3, 2, 5, e).
-piece(15, 14, 2, 3, 5, n).
-piece(15, 13, 1, 3, 3, e).
-piece(14, 16, 1, 6, 6, s).
-*/
+printPlayer(I) :- write('                   '), printPieces(I, 0, 0, 0, 0, 0, 0, 0, 0).
+
+printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B, N2B) :- (C == 8 -> (nl, (R == 1 -> (write('      Player '), print(I), write('     ')) ; write('                   ')), C1 is 0, R1 is R + 1, printPieces(I, C1, R1, N1T, N2T, N1N, N2N, N1B, N2B)) ;
+    ((R == 0 -> (N1T > 7 -> ! ; (N2T > 7 -> (N2T1 is 0, N1T1 is N1T + 1, printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; ((piece(N1T, N2T, I, P) -> C1 is C + 1, printPieceTop(P) ; C1 is C), N2T1 is N2T + 1, printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
+      ((R == 1 -> (N1N > 7 -> ! ; (N2N > 7 -> (N2N1 is 0, N1N1 is N1N + 1, printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; ((piece(N1N, N2N, I, P) -> C1 is C + 1, printPieceNumber(N1N, N2N, P) ; C1 is C), N2N1 is N2N + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
+        ((R == 2 -> (N1B > 7 -> ! ; (N2B > 7 -> (N2B1 is 0, N1B1 is N1B + 1, printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; ((piece(N1B, N2B, I, P) -> C1 is C + 1, printPieceBottom(P) ; C1 is C), N2B1 is N2B + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ;
+          ((R == 3 -> (N1T > 7 -> ! ; (N2T > 7 -> (N2T1 is 0, N1T1 is N1T + 1, printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; ((piece(N1T, N2T, I, P) -> C1 is C + 1, printPieceTop(P) ; C1 is C), N2T1 is N2T + 1, printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
+            ((R == 4 -> (N1N > 7 -> ! ; (N2N > 7 -> (N2N1 is 0, N1N1 is N1N + 1, printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; ((piece(N1N, N2N, I, P) -> C1 is C + 1, printPieceNumber(N1N, N2N, P) ; C1 is C), N2N1 is N2N + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
+              ((R == 5 -> (N1B > 7 -> ! ; (N2B > 7 -> (N2B1 is 0, N1B1 is N1B + 1, printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; ((piece(N1B, N2B, I, P) -> C1 is C + 1, printPieceBottom(P) ; C1 is C), N2B1 is N2B + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ; 
+                !))))))))))))).
+
+printPieceTop(P) :- (P == 0 -> write(' ┌───┬───┐') ; write('          ')).
+
+printPieceNumber(N1, N2, P) :- (P == 0 -> write(' | '), print(N1), write(' | '), print(N2), write(' |') ; write('          ')).
+
+printPieceBottom(P) :- (P == 0 -> write(' └───┴───┘') ; write('          ')).
+
+
+/*piece(Number1, Number2, Player, Played).*/
+
+piece(0, 1, 1, 0). 
+piece(0, 3, 1, 0). 
+piece(0, 4, 1, 0). 
+piece(0, 6, 1, 0). 
+piece(1, 1, 1, 1). 
+piece(1, 2, 1, 0). 
+piece(1, 4, 1, 0). 
+piece(1, 5, 1, 0). 
+piece(1, 7, 1, 0). 
+piece(2, 2, 1, 0). 
+piece(2, 3, 1, 1). 
+piece(2, 6, 1, 0). 
+piece(3, 3, 1, 1). 
+piece(3, 7, 1, 0). 
+piece(4, 5, 1, 0). 
+piece(5, 6, 1, 0). 
+piece(6, 7, 1, 0).
+piece(7, 7, 1, 1).
+
+piece(0, 0, 2, 0).
+piece(0, 2, 2, 0). 
+piece(0, 5, 2, 1). 
+piece(0, 7, 2, 0). 
+piece(1, 3, 2, 0). 
+piece(1, 6, 2, 0). 
+piece(2, 4, 2, 1). 
+piece(2, 5, 2, 1). 
+piece(2, 7, 2, 0). 
+piece(3, 4, 2, 0). 
+piece(3, 5, 2, 1). 
+piece(3, 6, 2, 0). 
+piece(4, 4, 2, 0). 
+piece(4, 6, 2, 0). 
+piece(4, 7, 2, 1). 
+piece(5, 5, 2, 0). 
+piece(5, 7, 2, 0). 
+piece(6, 6, 2, 1).
+
+
 /*halfPiece(Line, Column, Level, Number, Cardinal).*/
 
 halfPiece(12, 11, 1, 1, s).
