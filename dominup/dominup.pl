471b9f1b43284462bd9a79bde4a96204d73d472e
francisquinha <francisquinha@gmail.com>
2015-11-02 08:39:53 +0000

    check game over

--- dominup/dominup.pl
+++ dominup/dominup.pl
@@ -20,18 +20,18 @@ not(P) :- (P -> fail ; true).
 /* print board */
 /***************/
 
-printBoard :- nl, printLetters, nl, printGridTop, nl, printRows(1), printLetters, nl, nl,!.
+printBoard :- nl , printLetters , nl , printGridTop , nl , printRows(1) , printLetters , nl , nl , !.
 
 printRows(25) :- !.
-printRows(X) :- printLeftNumbers(X), printCells(X, 1), printRightNumbers(X), nl, printGrid(X), X1 is X + 1, nl, printRows(X1).
+printRows(X) :- printLeftNumbers(X) , printCells(X, 1) , printRightNumbers(X) , nl , printGrid(X) , X1 is X + 1 , nl , printRows(X1).
 
 printCells(_,25) :- !.
-printCells(X, Y) :- getTopLevel(X, Y, L), 
-                     (L > 0 -> halfPiece(X, Y, L, N, C), print(N), printCardinal(C), print(L); write('   ')), 
-                     write('│'), Y1 is Y + 1, printCells(X, Y1).
+printCells(X, Y) :- getTopLevel(X, Y, L) , 
+                     (L > 0 -> (halfPiece(X, Y, L, N, C) , print(N) , printCardinal(C) , print(L)) ; write('   ')) , 
+                     write('│') , Y1 is Y + 1 , printCells(X, Y1).
 
 getTopLevel(X, Y, 0) :- not(halfPiece(X, Y, _, _, _)).
-getTopLevel(X, Y, L) :- halfPiece(X, Y, L, _, _), L1 is L + 1, not(halfPiece(X, Y, L1, _, _)).
+getTopLevel(X, Y, L) :- halfPiece(X, Y, L, _, _) , L1 is L + 1 , not(halfPiece(X, Y, L1, _, _)).
 
 printCardinal(n) :- write('↑').
 printCardinal(e) :- write('→').
@@ -40,14 +40,14 @@ printCardinal(w) :- write('←').
 
 printLetters :- write('    A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   Y   X').
 
-printLeftNumbers(X) :- ((X < 10, write(0), write(X), write('│')) ; (write(X), write('│'))).
+printLeftNumbers(X) :- ((X < 10, write(0) , write(X) , write('│')) ; (write(X) , write('│'))).
 
-printRightNumbers(X) :- ((X < 10, write(0), write(X)) ; (write(X))).
+printRightNumbers(X) :- ((X < 10 , write(0), write(X)) ; (write(X))).
 
 printGridTop :- write('  ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐').
 
-printGrid(X) :-((X < 24, 
-                write('  ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤'));
+printGrid(X) :-((X < 24 , 
+                write('  ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤')) ;
                 write('  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘')).
 
 
@@ -55,40 +55,40 @@ printGrid(X) :-((X < 24,
 /* print player */
 /****************/
 
-printPlayer(I) :- write('         '), printPieces(I, 0, 0, 0, 0, 0, 0, 0, 0).
+printPlayer(I) :- write('         ') , printPieces(I, 0, 0, 0, 0, 0, 0, 0, 0).
 
 printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B, N2B) :- 
-        (C == 9 -> (nl, (R == 1 -> (write(' Player '), print(I)) ; write('         ')), 
-                    C1 is 0, R1 is R + 1, printPieces(I, C1, R1, N1T, N2T, N1N, N2N, N1B, N2B)) ; 
+        (C == 9 -> (nl , (R == 1 -> (write(' Player ') , print(I)) ; write('         ')) , 
+                    C1 is 0 , R1 is R + 1 , printPieces(I, C1, R1, N1T, N2T, N1N, N2N, N1B, N2B)) ; 
          ((R == 0 -> (N1T > 7 -> ! ; 
-                      (N2T > 7 -> (N2T1 is N1T + 1, N1T1 is N1T + 1, printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; 
-                       ((piece(N1T, N2T, I, P) -> C1 is C + 1, printPieceTop(P) ; C1 is C), 
-                        N2T1 is N2T + 1, printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
+                      (N2T > 7 -> (N2T1 is N1T + 1 , N1T1 is N1T + 1 , printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; 
+                       ((piece(N1T, N2T, I, P) -> C1 is C + 1 , printPieceTop(P) ; C1 is C), 
+                        N2T1 is N2T + 1 , printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
            ((R == 1 -> (N1N > 7 -> ! ; 
-                        (N2N > 7 -> (N2N1 is N1N + 1, N1N1 is N1N + 1, printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; 
-                         ((piece(N1N, N2N, I, P) -> C1 is C + 1, printPieceNumber(N1N, N2N, P) ; C1 is C), 
-                          N2N1 is N2N + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
+                        (N2N > 7 -> (N2N1 is N1N + 1 , N1N1 is N1N + 1 , printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; 
+                         ((piece(N1N, N2N, I, P) -> C1 is C + 1 , printPieceNumber(N1N, N2N, P) ; C1 is C), 
+                          N2N1 is N2N + 1 , printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
              ((R == 2 -> (N1B > 7 -> ! ; 
-                          (N2B > 7 -> (N2B1 is N1B + 1, N1B1 is N1B + 1, printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; 
-                           ((piece(N1B, N2B, I, P) -> C1 is C + 1, printPieceBottom(P) ; C1 is C), 
-                            N2B1 is N2B + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ;
+                          (N2B > 7 -> (N2B1 is N1B + 1 , N1B1 is N1B + 1 , printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; 
+                           ((piece(N1B, N2B, I, P) -> C1 is C + 1 , printPieceBottom(P) ; C1 is C), 
+                            N2B1 is N2B + 1 , printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ;
                ((R == 3 -> (N1T > 7 -> ! ; 
-                            (N2T > 7 -> (N2T1 is N1T + 1, N1T1 is N1T + 1, printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; 
-                             ((piece(N1T, N2T, I, P) -> C1 is C + 1, printPieceTop(P) ; C1 is C), 
-                              N2T1 is N2T + 1, printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
+                            (N2T > 7 -> (N2T1 is N1T + 1 , N1T1 is N1T + 1 , printPieces(I, C, R, N1T1, N2T1, N1N, N2N, N1B, N2B)) ; 
+                             ((piece(N1T, N2T, I, P) -> C1 is C + 1 , printPieceTop(P) ; C1 is C), 
+                              N2T1 is N2T + 1 , printPieces(I, C1, R, N1T, N2T1, N1N, N2N, N1B, N2B)))) ; 
                  ((R == 4 -> (N1N > 7 -> ! ; 
-                              (N2N > 7 -> (N2N1 is N1N + 1, N1N1 is N1N + 1, printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; 
-                               ((piece(N1N, N2N, I, P) -> C1 is C + 1, printPieceNumber(N1N, N2N, P) ; C1 is C), 
-                                N2N1 is N2N + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
+                              (N2N > 7 -> (N2N1 is N1N + 1 , N1N1 is N1N + 1 , printPieces(I, C, R, N1T, N2T, N1N1, N2N1, N1B, N2B)) ; 
+                               ((piece(N1N, N2N, I, P) -> C1 is C + 1 , printPieceNumber(N1N, N2N, P) ; C1 is C), 
+                                N2N1 is N2N + 1 , printPieces(I, C1, R, N1T, N2T, N1N, N2N1, N1B, N2B)))) ;
                    ((R == 5 -> (N1B > 7 -> ! ; 
-                                (N2B > 7 -> (N2B1 is N1B + 1, N1B1 is N1B + 1, printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; 
-                                 ((piece(N1B, N2B, I, P) -> C1 is C + 1, printPieceBottom(P) ; C1 is C), 
-                                  N2B1 is N2B + 1, printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ; 
+                                (N2B > 7 -> (N2B1 is N1B + 1 , N1B1 is N1B + 1 , printPieces(I, C, R, N1T, N2T, N1N, N2N, N1B1, N2B1)) ; 
+                                 ((piece(N1B, N2B, I, P) -> C1 is C + 1 , printPieceBottom(P) ; C1 is C), 
+                                  N2B1 is N2B + 1 , printPieces(I, C1, R, N1T, N2T, N1N, N2N, N1B, N2B1)))) ; 
                      !))))))))))))).
 
 printPieceTop(P) :- (P == 0 -> write(' ┌───┬───┐') ; write('          ')).
 
-printPieceNumber(N1, N2, P) :- (P == 0 -> write(' │ '), print(N1), write(' │ '), print(N2), write(' │') ; write('          ')).
+printPieceNumber(N1, N2, P) :- (P == 0 -> write(' │ ') , print(N1), write(' │ ') , print(N2) , write(' │') ; write('          ')).
 
 printPieceBottom(P) :- (P == 0 -> write(' └───┴───┘') ; write('          ')).
 
@@ -101,17 +101,17 @@ printPieceBottom(P) :- (P == 0 -> write(' └───┴───┘') ; write(
 :- dynamic piece/4.
 /* piece(Number1, Number2, Player, Played). */
 
-seedRandom :- now(B), X is B mod 30268 + 1, Y is B mod 30306 + 1, Z is B mod 30322 + 1, setrand(random(X, Y, Z, B)).
+seedRandom :- now(B) , X is B mod 30268 + 1 , Y is B mod 30306 + 1 , Z is B mod 30322 + 1 , setrand(random(X, Y, Z, B)).
 seedRandom.
 
-maybeRandom :- random(1, 3, I), (I == 1 -> true; fail). 
+maybeRandom :- random(1, 3, I) , (I == 1 -> true ; fail). 
 
 distributePieces(N1, N2, NI1, NI2) :- (N1 >=  7 -> assert(piece(N1, N2, 1, 0)) ; 
-                                       (N2 > 7 -> (N21 is N1 + 1, N11 is N1 + 1, distributePieces(N11, N21, NI1, NI2)) ;
-                                        (NI1 == 17 -> assert(piece(N1, N2, 2, 0)), NI21 is NI2 + 1, N21 is N2 + 1, distributePieces(N1, N21, NI1, NI21) ;
-                                         (NI2 == 18 -> assert(piece(N1, N2, 1, 0)), NI11 is NI1 + 1, N21 is N2 + 1, distributePieces(N1, N21, NI11, NI2) ;
-                                          (maybeRandom -> (assert(piece(N1, N2, 1, 0)), NI11 is NI1 + 1, N21 is N2 + 1, distributePieces(N1, N21, NI11, NI2)) ; 
-                                           (assert(piece(N1, N2, 2, 0)), NI21 is NI2 + 1, N21 is N2 + 1, distributePieces(N1, N21, NI1, NI21))))))).
+                                       (N2 > 7 -> (N21 is N1 + 1 , N11 is N1 + 1 , distributePieces(N11, N21, NI1, NI2)) ;
+                                        (NI1 == 17 -> assert(piece(N1, N2, 2, 0)) , NI21 is NI2 + 1 , N21 is N2 + 1 , distributePieces(N1, N21, NI1, NI21) ;
+                                         (NI2 == 18 -> assert(piece(N1, N2, 1, 0)) , NI11 is NI1 + 1 , N21 is N2 + 1 , distributePieces(N1, N21, NI11, NI2) ;
+                                          (maybeRandom -> (assert(piece(N1, N2, 1, 0)) , NI11 is NI1 + 1 , N21 is N2 + 1 , distributePieces(N1, N21, NI11, NI2)) ; 
+                                           (assert(piece(N1, N2, 2, 0)) , NI21 is NI2 + 1, N21 is N2 + 1 , distributePieces(N1, N21, NI1, NI21))))))).
 
 testDistribute :-
         assert(piece(0, 1, 1, 0)),
@@ -161,23 +161,24 @@ testDistribute :-
 /* halfPiece(Line, Column, Level, Number, Cardinal). */
 
 getOtherHalf(X1, Y1, n, X2, Y2, s) :- X2 is X1 - 1, Y2 is Y1. 
-getOtherHalf(X1, Y1, e, X2, Y2, w) :- X2 is X1, Y2 is Y1 + 1. 
+getOtherHalf(X1, Y1, e, X2, Y2, w) :- X2 is X1 , Y2 is Y1 + 1. 
 getOtherHalf(X1, Y1, s, X2, Y2, n) :- X2 is X1 + 1, Y2 is Y1. 
-getOtherHalf(X1, Y1, w, X2, Y2, e) :- X2 is X1, Y2 is Y1 - 1.
+getOtherHalf(X1, Y1, w, X2, Y2, e) :- X2 is X1 , Y2 is Y1 - 1.
 
 :- volatile playPiece/6.
 :- dynamic playPiece/6.
 /* playPiece(Number1, Number2, Player, Line, Column, Cardinal). */
 
-playPiece(N1, N2, I, X1, Y1, C1) :- getOtherHalf(X1, Y1, C1, X2, Y2, C2), getTopLevel(X1, Y1, L), checkPlay(N1, N2, I, X1, Y1, C1, X2, Y2, C2, L), L1 is L + 1, 
+playPiece(N1, N2, I, X1, Y1, C1) :- getOtherHalf(X1, Y1, C1, X2, Y2, C2) , getTopLevel(X1, Y1, L), 
+        checkPlay(N1, N2, I, X1, Y1, C1, X2, Y2, C2, L) , L1 is L + 1, 
         assert(halfPiece(X1, Y1, L1, N1, C1)), 
         assert(halfPiece(X2, Y2, L1, N2, C2)),
-        retract(piece(N1, N2, I, 0)), assert(piece(N1, N2, I, 1)).
+        retract(piece(N1, N2, I, 0)) , assert(piece(N1, N2, I, 1)).
 
-checkPlay(N1, N2, I, X1, Y1, C1, X2, Y2, C2, L) :- checkInsideBoard(X1, Y1, X2, Y2), checkPlayerPiece(N1, N2, I), checkLevelStable(L, X2, Y2), 
+checkPlay(N1, N2, I, X1, Y1, C1, X2, Y2, C2, L) :- checkInsideBoard(X1, Y1, X2, Y2) , checkPlayerPiece(N1, N2, I) , checkLevelStable(L, X2, Y2), 
         (L == 0 -> checkExpandOrthogonal(X1, Y1, C1, X2, Y2, C2) ; checkClimbNumbers(N1, X1, Y1, N2, X2, Y2, L)).
 
-checkInsideBoard(X1, Y1, X2, Y2) :- check1InsideBoard(X1), check1InsideBoard(Y1), check1InsideBoard(X2), check1InsideBoard(Y2).
+checkInsideBoard(X1, Y1, X2, Y2) :- check1InsideBoard(X1) , check1InsideBoard(Y1) , check1InsideBoard(X2) , check1InsideBoard(Y2).
 
 check1InsideBoard(Z) :- Z < 1 -> ! ; (Z > 24 -> ! ; true).
 
@@ -185,7 +186,7 @@ checkPlayerPiece(N1, N2, I) :- piece(N1, N2, I, 0).
 
 checkLevelStable(L, X2, Y2) :- getTopLevel(X2, Y2, L).
 
-checkClimbNumbers(N1, X1, Y1, N2, X2, Y2, L) :- halfPiece(X1, Y1, L, N1, _), halfPiece(X2, Y2, L, N2, _).
+checkClimbNumbers(N1, X1, Y1, N2, X2, Y2, L) :- halfPiece(X1, Y1, L, N1, _) , halfPiece(X2, Y2, L, N2, _).
 
 checkExpandOrthogonal(X1, Y1, C1, X2, Y2, C2) :- checkHalfOrthogonal(X1, Y1, C1) ; checkHalfOrthogonal(X2, Y2, C2).
 
@@ -243,3 +244,12 @@ halfPiece(14, 16, 1, 6, s).
 halfPiece(15, 16, 1, 6, n).
 */
 
+
+/***************/
+/* game engine */
+/***************/
+
+numberPieces(I, NP, N1, N2, R) :- N1 > 7 -> R is NP ; (N2 > 7 -> (N21 is N1 + 1 , N11 is N1 + 1 , numberPieces(I, NP, N11, N21, R)) ; 
+                                              (piece(N1, N2, I, 0) -> (NP1 is NP + 1 , N21 is N2 + 1, numberPieces(I, NP1, N1, N21, R)) ; 
+                                               (N21 is N2 + 1 , numberPieces(I, NP, N1, N21, R)))).
+checkGameOver(R) :- numberPieces(1, 0, 0, 0, R1), (R1 == 0 -> R is 1 ; (numberPieces(2, 0, 0, 0, R2), (R2 == 0 -> R is 2 ; R is 0))).
